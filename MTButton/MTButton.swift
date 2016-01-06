38ccf18b779b9b1b66b3d534a94a3f38a1222bba
Martin <tingyuanma@gmail.com>
2016-01-06 11:04:32 +0800

    first commit

---
+++ MTButton/MTButton.swift
@@ -0,0 +1,21 @@
+//
+//  MTButton.swift
+//  MTButton
+//
+//  Created by Martin on 1/6/16.
+//  Copyright © 2016 MT. All rights reserved.
+//
+
+import UIKit
+
+class MTButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
